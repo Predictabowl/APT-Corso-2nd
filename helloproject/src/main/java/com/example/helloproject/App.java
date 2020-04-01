cfb05c3056ddf49381b630d2d14b29dbc5335ac9
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-04-01 20:55:14 +0200

    Overloaded the sayHello method.
    Test was not update intentionally

--- helloproject/src/main/java/com/example/helloproject/App.java
+++ helloproject/src/main/java/com/example/helloproject/App.java
@@ -1,7 +1,14 @@
 package com.example.helloproject;
 
 public class App {
+	public String sayHello(String name) {
+		if(name == null)
+			return "Hello";
+		else
+			return "Hello" + name;
+	}
+	
 	public String sayHello() {
-		return "Hello";
+		return sayHello(null);
 	}
 }
